904f1ba88fcc7201876263e4c433bf588a1eb78d
Yankees4life <39246923+Yankees4life@users.noreply.github.com>
2021-11-18 03:56:32 -0500

    Update comchapter.bat

--- comchapter.bat
+++ comchapter.bat
@@ -3,9 +3,10 @@ REM Finding commercials
 comskip --ini=comskip.ini %1
 REM Adding chapters to file
 ffmpeg -i %1 -i "%~dpn1.ffmeta" -map_metadata 1 -c copy edit.ts
-rename /y edit.ts %1
+copy /y edit.ts %1
 REM Cleanup input files
 del /f "%~dpn1.txt"
 del /f "%~dpn1.log"
 del /f "%~dpn1.ffmeta"
 del /f "%~dpn1.edl"
+del /f edit.ts
